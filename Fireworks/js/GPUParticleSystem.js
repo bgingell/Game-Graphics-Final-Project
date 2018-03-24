34352ca2b839942ca7e8a53a27f8bd6ac69fd8e6
Eric Ventor <eventor@ucsc.edu>
2018-03-23 19:32:53 -0700

    made firework seem more relistic

--- Fireworks/js/GPUParticleSystem.js
+++ Fireworks/js/GPUParticleSystem.js
@@ -318,7 +318,7 @@ THREE.GPUParticleContainer = function ( maxParticles, particleSystem ) {
 
 		position = options.position !== undefined ? position.copy( options.position ) : position.set( 0, 0, 0 );
 		velocity = options.velocity !== undefined ? velocity.copy( options.velocity ) : velocity.set( 0, 0, 0 );
-		color = options.color !== undefined ? color.set( options.color ) : color.set( 0xffffff );
+		color = options.color !== undefined ? color.set( options.color ) : color.set( 0xffcc66 );
 
 		var positionRandomness = options.positionRandomness !== undefined ? options.positionRandomness : 0;
 		var velocityRandomness = options.velocityRandomness !== undefined ? options.velocityRandomness : 0;
